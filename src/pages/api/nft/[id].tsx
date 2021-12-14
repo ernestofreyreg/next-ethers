74b017477af69947d156f0faf201eeb6c297a317
Ernesto Freyre <ernestofreyreg@gmail.com>
2021-12-14 18:40:34 -0500

    added image

--- src/pages/api/nft/[id].tsx
+++ src/pages/api/nft/[id].tsx
@@ -1,4 +1,5 @@
 import Jimp from 'jimp'
+import { join } from 'path'
 // import { ethers } from 'ethers'
 import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
@@ -6,7 +7,7 @@ import nc from 'next-connect'
 export default nc<NextApiRequest, NextApiResponse>().get(async (req, res) => {
   const id = req.query.id as string
 
-  const image = await Jimp.read('./public/img/certificate.jpg')
+  const image = await Jimp.read(join(__dirname, 'public', 'certificate.jpg'))
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
   image.print(
     font,
