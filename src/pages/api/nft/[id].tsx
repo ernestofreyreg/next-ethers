c1c5066c0b601a4f6601ab48e136c960270c0070
Ernesto Freyre <ernestofreyreg@gmail.com>
2021-12-15 10:43:14 -0500

    working cert nft

--- src/pages/api/nft/[id].tsx
+++ src/pages/api/nft/[id].tsx
@@ -1,25 +1,39 @@
 import Jimp from 'jimp'
 import { join } from 'path'
-// import { ethers } from 'ethers'
+import { readFileSync } from 'fs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
 
 export default nc<NextApiRequest, NextApiResponse>().get(async (req, res) => {
   const id = req.query.id as string
 
-  const image = await Jimp.read(join(__dirname, 'public', 'img', 'certificate.jpg'))
+  const file = readFileSync('./public/img/certificate-bg.png')
+  const image = await Jimp.read(file)
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+  const fontSmall = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK)
   image.print(
     font,
     0,
     0,
     {
-      text: 'Hello world!' + id,
+      text: 'Learnpack NFT',
       alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
       alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
     },
-    1300,
-    765
+    842,
+    566
+  )
+  image.print(
+    fontSmall,
+    0,
+    32,
+    {
+      text: id,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+    },
+    842,
+    566
   )
 
   const buffer = await image.getBufferAsync(Jimp.MIME_JPEG)
